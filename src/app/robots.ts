489d4ac3a0266c3fd304e1ee4ebeda7a84ce167d
manunlockedx-creator <manunlockedx@gmail.com>
2026-04-13 10:38:01 -0700

    Add llms.txt and robots policy

---
+++ src/app/robots.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const rules = [
+    { userAgent: "*", allow: "/" },
+    { userAgent: "GPTBot", allow: "/" },
+    { userAgent: "Google-Extended", allow: "/" },
+    { userAgent: "PerplexityBot", allow: "/" },
+    { userAgent: "ClaudeBot", allow: "/" },
+    { userAgent: "Applebot-Extended", allow: "/" },
+  ];
+
+  return {
+    rules,
+    sitemap: "https://www.manunlocked.com/sitemap.xml",
+    host: "https://www.manunlocked.com",
+  };
+}
